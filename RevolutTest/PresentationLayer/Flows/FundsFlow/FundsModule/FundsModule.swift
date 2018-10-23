cc6bb4a316980a68b44d48bcb9185c2ebe6a1ce0
Egor Petrov <egor@barbatosso.com>
2018-10-23 22:25:33 +0300

    Created FundsModule

---
+++ RevolutTest/PresentationLayer/Flows/FundsFlow/FundsModule/FundsModule.swift
@@ -0,0 +1,11 @@
+//
+//  FundsModule.swift
+//  RevolutTest
+//
+//  Created by Egor Petrov on 23/10/2018.
+//  Copyright © 2018 Egor Petrov. All rights reserved.
+//
+
+import Foundation
+
+protocol FundsModule: Presentable { }
