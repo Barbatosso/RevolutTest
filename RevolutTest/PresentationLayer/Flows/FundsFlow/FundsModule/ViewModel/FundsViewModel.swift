5cbc5b6d695cbe503c3760b76fbc4bd80a8946e0
Egor Petrov <egor@barbatosso.com>
2018-10-24 16:33:24 +0300

    Added properties and funcs to the viewModel

--- RevolutTest/PresentationLayer/Flows/FundsFlow/FundsModule/ViewModel/FundsViewModel.swift
+++ RevolutTest/PresentationLayer/Flows/FundsFlow/FundsModule/ViewModel/FundsViewModel.swift
@@ -12,4 +12,39 @@ protocol FundsViewModel {
 
   var numberOfItems: Int { get }
   var items: [String: Double] { get }
+
+  func changeBaseFundCodeTo(_ code: String)
+  func startPollingFunds()
+}
+
+class FundsViewModelImpl: FundsViewModel {
+
+  var fundsService: FundsService
+
+  var numberOfItems: Int {
+    return items.count
+  }
+
+  var items: [String: Double]
+
+  private var timer: Timer?
+  private let fundCode = Observable<String>(value: "EUR")
+
+  init(fundsService: FundsService) {
+    self.fundsService = fundsService
+
+    items = [:]
+  }
+
+  func startPollingFunds() {
+    timer = Timer(timeInterval: 1.0, repeats: true) { [weak self] _ in
+      guard let strongSelf = self else { return }
+      strongSelf.fundsService.fetchFunds(for: strongSelf.fundCode.value) { funds, error in
+      }
+    }
+  }
+
+  func changeBaseFundCodeTo(_ code: String) {
+    fundCode.value = code
+  }
 }
