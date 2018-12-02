d80ef45a899b0ef442985e69a722aab1afa0617f
Egor Petrov <egor@barbatosso.com>
2018-12-02 16:26:17 +0300

    Created TableViewCellRegistrar

---
+++ RevolutTest/PresentationLayer/Common/TableManager/TableViewCellRegistrar.swift
@@ -0,0 +1,32 @@
+//
+//  TableViewCellRegistrar.swift
+//  RevolutTest
+//
+//  Created by Egor Petrov on 02/12/2018.
+//  Copyright © 2018 Egor Petrov. All rights reserved.
+//
+
+import UIKit
+
+protocol TableViewCellRegistrar {
+
+  func register(_ cellClass: AnyClass, for reuseId: String)
+}
+
+class TableViewCellRegistrarImpl: TableViewCellRegistrar {
+
+  private var registeredIds = Set<String>()
+
+  private weak var tableView: UITableView?
+
+  init(tableView: UITableView?) {
+    self.tableView = tableView
+  }
+
+  func register(_ cellClass: AnyClass, for reuseId: String) {
+    guard !registeredIds.contains(reuseId) else { return }
+
+    registeredIds.insert(reuseId)
+    tableView?.register(cellClass, forCellReuseIdentifier: reuseId)
+  }
+}
